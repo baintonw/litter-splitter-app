202223cead0230ea67591b1f64da5b76358f0a1d
Will Bainton <baintonw@gmail.com>
2019-06-17 10:20:25 -0400

    Migrated-successfully

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,5 @@
 class Cat < ApplicationRecord
   belongs_to :user
+  has_many :claims
+  has_many :users, through: :claims
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-  has_may :cats
+  has_many :cats
+  has_many :claims
+  
+
+  # def selected_cats
+  #   self.claims.map { |claim|
+  #       claim.cat
+  #     }
+  # end
 end
